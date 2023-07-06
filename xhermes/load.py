b9e51626d9f5567dd02265ea729e69a2e1dc41d0
Mike Kryjak <mikekryjak@gmail.com>
2023-07-06 10:40:05 +0100

    Add coordinate unnormalisation

--- xhermes/load.py
+++ xhermes/load.py
@@ -44,17 +44,15 @@ def open_hermesdataset(
     rho_s0 = meta["rho_s0"]
     Nnorm = meta["Nnorm"]
     Tnorm = meta["Tnorm"]
+    Bnorm = meta["Bnorm"]
 
     # SI values
     Mp = 1.67e-27  # Proton mass
     e = 1.602e-19  # Coulombs
 
-    # Coordinates
-    ds.t.attrs["units_type"] = "hermes"
-    ds.t.attrs["units"] = "s"
-    ds.t.attrs["conversion"] = 1.0 / Omega_ci
 
-    for varname in ds:
+
+    for varname in list(ds.data_vars) + list(ds.coords):
         da = ds[varname]
         if len(da.dims) == 4:  # Time-evolving field
             da.attrs["units_type"] = "hermes"
@@ -134,6 +132,70 @@ def open_hermesdataset(
             else:
                 # Don't know what this is
                 da.attrs["units_type"] = "unknown"
+
+                
+                
+        # Coordinates
+        if varname == "dy":
+            # Poloidal cell angular width
+            da.attrs.update(
+                {
+                    "units": "radian",
+                    "conversion": 1,
+                    "standard_name": "poloidal cell angular width",
+                    "long_name": "Poloidal cell angular width",
+                }
+            )
+        elif varname == "dx":
+            # Radial cell width
+            da.attrs.update(
+                {
+                    "units": "Wb",
+                    "conversion": rho_s0**2 * Bnorm,
+                    "standard_name": "radial cell width",
+                    "long_name": "Radial cell width in flux space",
+                }
+            )
+        elif varname == "J":
+            # Jacobian
+            da.attrs.update(
+                {
+                    "units": "m/radian T",
+                    "conversion": rho_s0 / Bnorm,
+                    "standard_name": "Jacobian",
+                    "long_name": "Jacobian to translate from flux to cylindrical coordinates in real space",
+                }
+            )
+        elif varname == "g_22":
+            # Metric tensor term
+            da.attrs.update(
+                {
+                    "units": "m2",
+                    "conversion": rho_s0**2,
+                    "standard_name": "g_22",
+                    "long_name": "g_22 term in the metric tensor",
+                }
+            )
+        elif varname == "g11":
+            # Metric tensor term
+            da.attrs.update(
+                {
+                    "units": "T-2m-2",
+                    "conversion": (Bnorm * rho_s0)**2,
+                    "standard_name": "g11",
+                    "long_name": "g11 term in the metric tensor",
+                }
+            )
+        elif varname == "t":
+            # Time
+            da.attrs.update(
+                {
+                    "units": "s",
+                    "conversion": 1/Omega_ci,
+                    "standard_name": "time",
+                    "long_name": "Time",
+                }
+            )
 
     if unnormalise:
         ds.hermes.unnormalise()
