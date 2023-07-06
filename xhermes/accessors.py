4d86e29f5a68ca0f527f97d5a158f89f14591530
Mike Kryjak <mikekryjak@gmail.com>
2023-07-06 11:37:54 +0100

    Add 2D tokamak geometry extraction

--- xhermes/accessors.py
+++ xhermes/accessors.py
@@ -95,6 +95,123 @@ class HermesDatasetAccessor(BoutDatasetAccessor):
             "long_name" : "Cell Volume"})
         
         return ds
+    
+    def extract_2d_tokamak_geometry(self):
+        """
+        Process the results to generate 2D relevant geometry data:
+        
+        Calculates
+        ----------
+        - nxg and nyg, versions of ny and nx which always include guard cells 
+          if they exist in the dataset, and do not include them if they don't
+        - j1_1g, j1_2g, ..., versions of jyseps which account for guards in the 
+          same way as nxg and nyg
+        - x_idx, y_idx: arrays of radial and poloidal indices of all cells
+        - dv, dr, dthe, dl: cell volume and real space cell dimensions
+        - Adds target names to the metadata accounting for single or double null
+        
+        Notes
+        ----------
+        - Adds copies of jyseps1_1 that are shortened to j1_1 etc. 
+          This is potentially annoying.
+        - The reason it's useful to have arrays of coordinate indices
+          is because Xarray is surprisingly awkward when it comes to 
+          obtaining this from the coordinates.
+        - This method has a hardcoded requirement for providing the grid file.
+          
+        Returns
+        ----------
+        - Dataset with the new geometry 
+        """
+        
+        ds = self.data.squeeze()
+        m = ds.metadata
+        
+        if "topology" not in m:
+            raise Exception(
+                "2D Tokamak topology missing from metadata. Please load model with the flag geometry = 'toroidal' and provide grid")
+        
+        # Add theta index to coords so that both X and theta can be accessed index-wise
+        # It is surprisingly hard to extract the index of coordinates in Xarray...
+        ds.coords["theta_idx"] = (["theta"], range(len(ds.coords["theta"])))
+        
+        # Extract target names. This is done here and not in load because load is not 
+        # tokamak specific.
+        if "single-null" in m["topology"]:
+            m["targets"] = ["inner_lower", "outer_lower"]
+        elif "double-null" in m["topology"]:
+            m["targets"] = ["inner_lower", "outer_lower", "inner_upper", "outer_upper"]
+            
+        num_targets = len(m["targets"])
+
+        # nyg, nxg: cell counts which are always with guard cells 
+        # if they exist, or not if they don't
+        if m["keep_xboundaries"] == 0:
+            m["nxg"] = m["nx"] - m["MXG"] * 2 
+            m["MXG"] = 0
+        else:
+            m["nxg"] = m["nx"]
+            
+        if m["keep_yboundaries"] == 0:
+            m["nyg"] = m["ny"]    
+            m["MYG"] = 0
+        else:
+            m["nyg"] = m["ny"] + m["MYG"] * num_targets   
+        
+        # Simplified names of the separatrix indices          
+        m["j1_1"] = m["jyseps1_1"]
+        m["j1_2"] = m["jyseps1_2"]
+        m["j2_1"] = m["jyseps2_1"]
+        m["j2_2"] = m["jyseps2_2"]
+        
+        # Separatrix indices which account for guard cells in the same way as nxg, nyg
+        m["j1_1g"] = m["j1_1"] + m["MYG"]
+        m["j1_2g"] = m["j1_2"] + m["MYG"] * (num_targets - 1)
+        m["j2_1g"] = m["j2_1"] + m["MYG"]
+        m["j2_2g"] = m["j2_2"] + m["MYG"] * (num_targets - 1)
+            
+        # Array of radial (x) indices and of poloidal (y) indices for each cell
+        # This is useful because Xarray makes it awkward to extract indices in certain cases
+        ds["x_idx"] = (["x", "theta"], np.array([np.array(range(m["nxg"]))] * int(m["nyg"])).transpose())
+        ds["y_idx"] = (["x", "theta"], np.array([np.array(range(m["nyg"]))] * int(m["nxg"])))
+        
+        # Cell volume calculation
+        ds["dv"] = (["x", "theta"], ds["dx"].data * ds["dy"].data * ds["dz"].data * ds["J"].data)
+        ds["dv"].attrs.update({
+            "conversion" : 1,
+            "units" : "m3",
+            "standard_name" : "cell volume",
+            "long_name" : "Cell volume",
+            "source" : "xHermes"})
+        
+        # Cell areas in real space - comes from Jacobian
+        # Note: can be calculated from flux space or real space coordinates:
+        # dV = (hthe/Bpol) * (R*Bpol*dr) * dy*2pi = hthe * dy * dr * 2pi * R
+        ds["dr"] = (["x", "theta"], ds.dx.data / (ds.R.data * ds.Bpxy.data))
+        ds["dr"].attrs.update({
+            "conversion" : 1,
+            "units" : "m",
+            "standard_name" : "radial length",
+            "long_name" : "Length of cell in the radial direction",
+            "source" : "xHermes"})
+        
+        ds["hthe"] = (["x", "theta"], ds["J"].data * ds["Bpxy"].data)    # h_theta
+        ds["hthe"].attrs.update({
+            "conversion" : 1,
+            "units" : "m/radian",
+            "standard_name" : "h_theta: poloidal arc length per radian",
+            "long_name" : "h_theta: poloidal arc length per radian",
+            "source" : "xHermes"})
+        
+        ds["dl"] = (["x", "theta"], ds["dy"].data * ds["hthe"].data)    # poloidal arc length
+        ds["dl"].attrs.update({
+            "conversion" : 1,
+            "units" : "m",
+            "standard_name" : "poloidal arc length",
+            "long_name" : "Poloidal arc length",
+            "source" : "xHermes"})
+        
+        return ds
 
 
 @register_dataarray_accessor("hermes")
