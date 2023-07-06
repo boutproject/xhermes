410e38119f4cd5bb44d70fc11ed15d1f9cd2b0b9
Mike Kryjak <mikekryjak@gmail.com>
2023-07-06 11:10:34 +0100

    Add 1D tokamak geom extraction - Note it's not in place. Can't figure out how to do it.

--- xhermes/accessors.py
+++ xhermes/accessors.py
@@ -1,5 +1,6 @@
 from xarray import register_dataset_accessor, register_dataarray_accessor
 from xbout import BoutDatasetAccessor, BoutDataArrayAccessor
+import numpy as np
 
 
 @register_dataset_accessor("hermes")
@@ -36,6 +37,64 @@ class HermesDatasetAccessor(BoutDatasetAccessor):
                 self.data[coord].attrs["units_type"] = "SI"
             else:
                 raise ValueError("Unrecognised units_type: " + units_type)
+            
+    def extract_1d_tokamak_geometry(self):
+        """
+        Process the results to generate 1D relevant geometry data:
+        - Reconstruct pos, the cell position in [m] from upstream from dy
+        - Calculate cross-sectional area da and volume dv
+
+        Notes
+        ----------
+        - dy is technically in flux space, but in 1D 
+          this is the same as in regular space and in units of [m]
+          
+        Returns
+        ----------
+        - Dataset with the new geometry 
+        """
+        ds = self.data.squeeze() # Get rid of 1-length dimensions
+
+        # Reconstruct grid position (pos, as in position) from dy
+        dy = ds.coords["dy"].values
+        n = len(dy)
+        pos = np.zeros(n)
+        pos[0] = -0.5*dy[1]
+        pos[1] = 0.5*dy[1]
+
+        for i in range(2,n):
+            pos[i] = pos[i-1] + 0.5*dy[i-1] + 0.5*dy[i]
+
+        # Guard replace to get position at boundaries
+        pos[-2] = (pos[-3] + pos[-2])/2
+        pos[1] = (pos[1] + pos[2])/2 
+
+        # Set 0 to be at first cell boundary in domain
+        pos = pos - pos[1]
+        ds["pos"] = (["y"], pos)
+        
+        # Make pos the main coordinate instead of y
+        ds = ds.swap_dims({"y":"pos"})
+        ds.coords["pos"].attrs = ds.coords["y"].attrs
+
+        # Derive and append metadata for the cross-sectional area
+        # and volume. The conversions are 1 because the derivation
+        # is from already-normalised parameters
+        ds["da"] = ds.J / np.sqrt(ds.g_22)
+        ds["da"].attrs.update({
+            "conversion" : 1,
+            "units" : "m2",
+            "standard_name" : "cross-sectional area",
+            "long_name" : "Cell parallel cross-sectional area"})
+        
+        ds["dv"] = ds.J * ds.dy
+        ds["dv"].attrs.update({
+            "conversion" : 1,
+            "units" : "m3",
+            "standard_name" : "cell volume",
+            "long_name" : "Cell Volume"})
+        
+        return ds
 
 
 @register_dataarray_accessor("hermes")
