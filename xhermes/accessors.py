84124c19286c8ed3aaa1fb5d25961b41b29862e1
Mike Kryjak <mikekryjak@gmail.com>
2024-05-02 10:09:12 +0100

    Fix 1D da/dv when dz or dx != 1

--- xhermes/accessors.py
+++ xhermes/accessors.py
@@ -80,14 +80,14 @@ class HermesDatasetAccessor(BoutDatasetAccessor):
         # Derive and append metadata for the cross-sectional area
         # and volume. The conversions are 1 because the derivation
         # is from already-normalised parameters
-        ds["da"] = ds.J / np.sqrt(ds.g_22)
+        ds["da"] = ds.dx * ds.dz * ds.J / np.sqrt(ds.g_22)
         ds["da"].attrs.update({
             "conversion" : 1,
             "units" : "m2",
             "standard_name" : "cross-sectional area",
             "long_name" : "Cell parallel cross-sectional area"})
         
-        ds["dv"] = ds.J * ds.dy
+        ds["dv"] = ds.J * ds.dx * ds.dy * ds.dz 
         ds["dv"].attrs.update({
             "conversion" : 1,
             "units" : "m3",
