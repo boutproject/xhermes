b4440bff107ae4e78458a443d5e1c1d325cb7061
Mike Kryjak <mikekryjak@gmail.com>
2025-11-27 11:05:04 +0000

    Rename j1_1g, add ny_innerg ny_innerg makes selecting easier. It's always defined as the index of the outer upper target in double null.

--- xhermes/selectors.py
+++ xhermes/selectors.py
@@ -20,22 +20,17 @@ def poloidal_selector(ds, name):
     
     m = ds.metadata
     # TODO: use j1_1g etc from xBOUT once it's implemented there
-    j1_1g = m["j1_1g"]
-    j1_2g = m["j1_2g"]
-    j2_1g = m["j2_1g"]
-    j2_2g = m["j2_2g"]
+    j1_1g = m["jyseps1_1g"]
+    j1_2g = m["jyseps1_2g"]
+    j2_1g = m["jyseps2_1g"]
+    j2_2g = m["jyseps2_2g"]
     MXG = m["MXG"]
     MYG = m["MYG"]
     nxg = m["nxg"]
     nyg = m["nyg"]
-    ny_inner = m["ny_inner"]
+    ny_innerg = m["ny_innerg"]
     topology = m["topology"]
     
-    # Inverse of MYG needed for some double null indexing
-    if MYG == 0:
-        MYG_inverse = 2
-    else:
-        MYG_inverse = 0
     
     index = {}
     
@@ -55,27 +50,28 @@ def poloidal_selector(ds, name):
     elif "double-null" in topology:
         index["inner_lower_target"] = MYG
         index["outer_lower_target"] = nyg - MYG - 1
-        index["inner_upper_target"] = ny_inner - MYG_inverse + 1
-        index["outer_upper_target"] = ny_inner + MYG * 3
+        index["inner_upper_target"] = ny_innerg - MYG * 2 -1 
+        index["outer_upper_target"] = ny_innerg 
 
     # Guard selection
-    if MYG > 0:
-        if "single-null" in topology:
-            index["yguards"] = np.r_[
-                slice(None,MYG),
-                slice(nyg-MYG, nyg)
-            ]
-        
+    if "guard" in name:
+        if MYG > 0:
+            if "single-null" in topology:
+                index["yguards"] = np.r_[
+                    slice(None,MYG),
+                    slice(nyg-MYG, nyg)
+                ]
+            
+            else:
+                index["yguards"] = np.r_[
+                    slice(None,MYG),
+                    slice(ny_innerg - MYG * 2, ny_innerg - MYG),
+                    slice(ny_innerg - MYG, ny_innerg),
+                    slice(nyg-MYG, nyg)
+                ]
+            
         else:
-            index["yguards"] = np.r_[
-                slice(None,MYG),
-                slice(ny_inner+MYG, ny_inner + MYG*2),
-                slice(ny_inner+MYG*2, ny_inner + MYG*3),
-                slice(nyg-MYG, nyg)
-            ]
-           
-    else:
-        raise ValueError("Cannot select guards - no guards found in domain!")
+            raise ValueError("Cannot select guards - no guards found in domain!")
         
     
     return index[name]
@@ -101,15 +97,15 @@ def region_selector(ds, name):
     
     m = ds.metadata
     # TODO: use j1_1g etc from xBOUT once it's implemented there
-    j1_1g = m["j1_1g"]
-    j1_2g = m["j1_2g"]
-    j2_1g = m["j2_1g"]
-    j2_2g = m["j2_2g"]
+    j1_1g = m["jyseps1_1g"]
+    j1_2g = m["jyseps1_2g"]
+    j2_1g = m["jyseps2_1g"]
+    j2_2g = m["jyseps2_2g"]
     MXG = m["MXG"]
     MYG = m["MYG"]
     nxg = m["nxg"]
     nyg = m["nyg"]
-    ny_inner = m["ny_inner"]
+    ny_innerg = m["ny_innerg"]
     topology = m["topology"]
     
     
