4501c1c2c5989ed3a02b9e1d54851fa1b23729f4
Mike Kryjak <mikekryjak@gmail.com>
2026-04-02 14:13:43 +0100

    Improve test_slice_poloidal Now no longer relies on try/except for selections which are unavailable in a particular topology, which was bad design. Also correctly saves and retrieves the reference data.

--- tests/test_slice_poloidal.py
+++ tests/test_slice_poloidal.py
@@ -12,9 +12,18 @@ from urllib.request import urlretrieve
 import zipfile
 from time import time
 
-t0 = time()
 
-RCOORDS_FILE = Path(__file__).parent / "Rcoords_expected.json"
+###
+# This test checks all the poloidal selections from slice_poloidal against 10 grids of 
+# different topologies and guard cell configurations. The expected R coordinates of the selected
+# cells are stored in a JSON file and compared to the test output. The test can also be used to
+# generate the expected data by setting generate_data = True, which will save the R coordinates
+# of the selected cells for all grids to the JSON file. The test also generates plots of
+# the logical and poloidal grid with the selected region highlighted for visual inspection, which
+# can be saved by setting plot = True. 
+###
+
+RCOORDS_FILE = Path(__file__).parent / "test_slice_poloidal_reference.json"
 
 if RCOORDS_FILE.exists():
     with open(RCOORDS_FILE) as f:
@@ -38,140 +47,143 @@ def example_grids(tmp_path_factory):
         
     return tmp
 
-poloidal_selections = [
-        # "inner_target",
-        # "outer_target",
-        # "inner_lower_target",
-        # "inner_upper_target",
-        # "outer_lower_target",
-        # "outer_upper_target",
-        # "inner_lower_midplane",
-        # "inner_upper_midplane",
-        # "outer_lower_midplane",
-        # "outer_upper_midplane",
-        # "inner_xpoint",
-        # "outer_xpoint",
-        # "inner_lower_xpoint",
-        # "inner_upper_xpoint",
-        # "outer_upper_xpoint",
-        # "outer_lower_xpoint",
-        # "inner_lower_sol",
-        # "inner_upper_sol",
-        # "outer_upper_sol",
-        # "outer_lower_sol",
-        # "inner_sol",
-        # "outer_sol",
-        # "sol",
-        # "inner_core",
-        # "outer_core",
-        # "core",
-        # "inner_lower_divertor",
-        # "inner_upper_divertor",
-        # "outer_upper_divertor",
-        # "outer_lower_divertor",
+poloidal_selections = {}
 
-        # "inner_divertor",
-        # "outer_divertor",
-
-        # "lower_pfr",
-        # "upper_pfr",
-        # "pfr",
-        # "pfr",
+poloidal_selections["sn"] = [
+        "inner_target",
+        "outer_target",
+        "inner_xpoint",
+        "outer_xpoint",
+        "inner_sol",
+        "outer_sol",
+        "inner_sol_extra",
+        "outer_sol_extra",
+        "sol",
+        "core",
+        "inner_divertor",
+        "outer_divertor",
+        "pfr",
         "yguards",
-    ]
+]
+
+poloidal_selections["dn"] = [
+        "inner_lower_target",
+        "inner_upper_target",
+        "outer_lower_target",
+        "outer_upper_target",
+        "inner_lower_midplane",
+        "inner_upper_midplane",
+        "outer_lower_midplane",
+        "outer_upper_midplane",
+        "inner_lower_xpoint",
+        "inner_upper_xpoint",
+        "outer_upper_xpoint",
+        "outer_lower_xpoint",
+        "inner_lower_sol_extra",
+        "inner_upper_sol_extra",
+        "outer_upper_sol_extra",
+        "outer_lower_sol_extra",
+        "inner_lower_sol",
+        "inner_upper_sol",
+        "outer_upper_sol",
+        "outer_lower_sol",
+        "sol",
+        "inner_core",
+        "outer_core",
+        "core",
+        "inner_lower_divertor",
+        "inner_upper_divertor",
+        "outer_upper_divertor",
+        "outer_lower_divertor",
+        "lower_pfr",
+        "upper_pfr",
+        "pfr",
+        "yguards",
+]
+
+
 
 plot = False
-generate_data = True
+generate_data = False
 
 
 def test_selectors(example_grids):
-    
-    grids = dict(
-        cdn = example_grids / "example_cdn.grd.nc",
-        cdn_noguards = example_grids / "example_cdn_noguards.grd.nc",
-        ldn = example_grids / "example_ldn.grd.nc",
-        ldn_noguards = example_grids / "example_ldn_noguards.grd.nc",
+
+    all_grids = dict(
+    sn_grids = dict(
         lsn = example_grids / "example_lsn.grd.nc",
         lsn_noguards = example_grids / "example_lsn_noguards.grd.nc",
         usn = example_grids / "example_usn.grd.nc",
         usn_noguards = example_grids / "example_usn_noguards.grd.nc",
+    ),
+
+    dn_grids = dict(
+        cdn = example_grids / "example_cdn.grd.nc",
+        cdn_noguards = example_grids / "example_cdn_noguards.grd.nc",
+        ldn = example_grids / "example_ldn.grd.nc",
+        ldn_noguards = example_grids / "example_ldn_noguards.grd.nc",
         udn2 = example_grids / "example_udn2.grd.nc",
         udn2_noguards = example_grids / "example_udn2_noguards.grd.nc",
     )
-
+)    
+    
     if plot:
         output_dir = Path("poloidal_selection_images")
         output_dir.mkdir(exist_ok=True)
 
-    nrows = len(grids)
-    print()
 
-    for i, selection in enumerate(poloidal_selections):
-        print(f"{selection}")
+    for topology_type in ["sn", "dn"]:
+        print(f"Testing {topology_type} selectors...")
+        grids = all_grids[f"{topology_type}_grids"]
+        nrows = len(grids)
 
-        if plot:
-            fig = plt.figure(figsize=(6, nrows * 4))
-            gs = fig.add_gridspec(nrows, 2, width_ratios=[1, 1], hspace=0.5, wspace=0.1)
-
-        for row, (name, path) in enumerate(grids.items()):
-            print(f"{name} ", end="")
-            
-            ds = xhermes.HypnotoadGrid(path)
+        for i, selection in enumerate(poloidal_selections[topology_type]):
 
             
-            # Generate plots for 
+
+            print(f"{selection}")
+
             if plot:
-                ax0 = fig.add_subplot(gs[row, 0])
-                ax1 = fig.add_subplot(gs[row, 1])
-                
-                try:
+                fig = plt.figure(figsize=(6, nrows * 4))
+                gs = fig.add_gridspec(nrows, 2, width_ratios=[1, 1], hspace=0.5, wspace=0.1)
+
+            for row, (grid_name, path) in enumerate(grids.items()):
+                if "guards" in selection and "noguards" in grid_name:
+                    continue
+                print(f"{grid_name} ", end="")
+                ds = xhermes.HypnotoadGrid(path)
+
+                # Generate plots for visual check
+                if plot:
+                    ax0 = fig.add_subplot(gs[row, 0])
+                    ax1 = fig.add_subplot(gs[row, 1])
                     xhermes.plot_selection(
                         ds,
                         selection=(slice(None), xhermes.slice_poloidal(ds, selection)),
                         axes=[ax0, ax1]
                     )
-                    ax0.set_title(f"{name}\nLogical")
-                    ax1.set_title(f"{name}\nPoloidal")
+                    ax0.set_title(f"{grid_name}\nLogical")
+                    ax1.set_title(f"{grid_name}\nPoloidal")
+                        
+                # Generate test data and compare to expected
+                R_test = ds["Rxy"][slice(None), xhermes.slice_poloidal(ds, selection)]
 
-                except Exception as e:
-                    print("Failed! ", end="")
-                    msg = textwrap.fill(str(e), width=40)
-                    ax0.text(0.0, 0.5, msg, fontsize="medium", transform=ax0.transAxes,
-                            va="center", ha="left", clip_on=True)
-                    
+                if generate_data:
+                    if selection not in Rcoords:
+                        Rcoords[selection] = {}
+                    Rcoords[selection][grid_name] = R_test.tolist()
+                else:
+                    R_expected = Rcoords[selection][grid_name]
+                    npt.assert_array_equal(R_test, R_expected,
+                                        err_msg=f"Selector mismatch for {selection} in {grid_name}!")
 
-            try:
-                R_test = xhermes.slice_poloidal(ds, selection)["Rxy"].values.flatten()
-            except Exception as e:
-                print(f"Failed ({e}) ", end="")
-                continue
-
-            if generate_data:
-                if selection not in Rcoords:
-                    Rcoords[selection] = {}
-                Rcoords[selection][name] = R_test.tolist()
-            else:
-                R_expected = Rcoords[selection][name]
-                npt.assert_array_equal(R_test, R_expected,
-                                       err_msg=f"Selector mismatch for {selection} in {name}!")
-
-        print()
-
-        if plot:
-            fig.savefig(output_dir / f"{selection}.png", dpi=300, bbox_inches="tight")
-            plt.close(fig)
+            if plot:
+                fig.savefig(output_dir / f"{selection}-{topology_type}.png", dpi=300, bbox_inches="tight")
+                plt.close(fig)
 
     if generate_data:
         with open(RCOORDS_FILE, "w") as f:
             json.dump(Rcoords, f, indent=2)
         print(f"Saved expected data to {RCOORDS_FILE}")
         
-        # for selection in Rcoords.keys():
-        #     for geometry in Rcoords[selection].keys():
-        #         expected = Rcoords[selection][geometry]
-        #         ds = xhermes.HypnotoadGrid(grids[geometry])
-        #         test = ds["Rxy"][xhermes.slice_2d(ds, selection)]
-                
-        #         npt.assert_array_equal(test, expected, 
-        #                                     err_msg=f"Selector mismatch for {selection} in {geometry}!")
             
